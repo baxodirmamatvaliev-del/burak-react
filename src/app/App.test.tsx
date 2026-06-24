6aa6f48a7474bd9e9ae30f61427ab638f955dfca
LEO <baxodirmamatvaliev@gmail.com>
2026-06-25 04:31:56 +0900

    BRR: Initial Setup

---
+++ src/app/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders home text', () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText(/home/i)).toBeInTheDocument();
+});
