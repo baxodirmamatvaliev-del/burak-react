b596ea4eab5a0fe7f70aab79cd692afa3cea3371
LEO <baxodirmamatvaliev@gmail.com>
2026-06-27 01:39:23 +0900

    fix: organize header and footer components

---
+++ src/app/components/headers/HomeNavbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function HomeNavbar () {
+    return <div>Home Navbar</div>
+}
---
+++ src/app/components/headers/OtherNavbar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function OtherNavbar () {
+    return <div>Other Navbar </div>
+}
