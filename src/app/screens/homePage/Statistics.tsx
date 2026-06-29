e1abcde7d2224a88252df0e68778e361bcca96d3
LEO <baxodirmamatvaliev@gmail.com>
2026-06-30 05:10:59 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/Statistics.tsx
+++ src/app/screens/homePage/Statistics.tsx
@@ -1,10 +1,37 @@
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
+import Divider from "../../components/divider";
 
 export function Statistics(){
     return( 
     <div>
-     <Container>statistics</Container>
+     <Container className={"static-frame"} >
+      <Stack className={"info"}>
+        <Stack className={"stattic-box"}>
+        <Box className={"stattic-box"}>12</Box>
+        <Box className={"stattic-text"}>Restaurants</Box>
+      </Stack>
+
+     <Divider height="64" width="2" bg="#E3C08D" />
+
+       <Stack className={"stattic-box"}>
+        <Box className={"stattic-box"}>8</Box>
+        <Box className={"stattic-text"}>Experoence</Box>
+      </Stack>
+       <Divider height="64" width="2" bg="#E3C08D" />
+
+       <Stack className={"stattic-box"}>
+        <Box className={"stattic-box"}>59+</Box>
+        <Box className={"stattic-text"}>Menu</Box>
+      </Stack>
+      <Divider height="64" width="2" bg="#E3C08D" />
+
+       <Stack className={"stattic-box"}>
+        <Box className={"stattic-box"}>200+</Box>
+        <Box className={"stattic-text"}>Clients</Box>
+      </Stack>
+      </Stack>
+     </Container>
     </div> 
     );
 }
