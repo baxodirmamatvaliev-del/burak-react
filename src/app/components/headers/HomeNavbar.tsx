421b57c74e3428824dcab3b7c850690467aab998
LEO <baxodirmamatvaliev@gmail.com>
2026-06-29 22:05:02 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -44,7 +44,21 @@ export function HomeNavbar () {
     )}
       </Stack>
     </Stack>
-    <Stack>DETAIL</Stack>
+    <Stack className={"header-frame"}>
+        <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+            World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"} >The Choise, not just a choice</Box>
+            <Box className={"service-txt"} >24 hours service</Box>
+            <Box className={"signup"}>
+                {!authMember ? (<Button variant={"contained"} className={"sigup-botton"}> SIGN UP</Button>): null}
+            </Box>
+        </Stack>
+        <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+        </Box>       
+    </Stack>
   </Container>
 </div>
 );
