c4193fc4cd78555df984a83b4d80714bcdc3cc53
LEO <baxodirmamatvaliev@gmail.com>
2026-07-01 22:21:05 +0900

    feat: develop events sectional component

---
+++ src/swiper.d.ts
@@ -0,0 +1,12 @@
+declare module "swiper/react" {
+  export const Swiper: any;
+  export const SwiperSlide: any;
+}
+
+declare module "swiper" {
+  const SwiperCore: any;
+  export const Autoplay: any;
+  export const Navigation: any;
+  export const Pagination: any;
+  export default SwiperCore;
+}
