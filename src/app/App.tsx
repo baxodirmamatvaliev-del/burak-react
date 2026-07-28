37dcf6faf4c7aef99e19d47a901b40feb9ff9834
LEO <baxodirmamatvaliev@gmail.com>
2026-07-28 13:41:08 +0900

    feat: publish authentication model component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,27 @@ import  UserPage from './screens/userPage';
 import  HomeNavbar  from './components/headers/HomeNavbar';
 import  OtherNavbar  from './components/headers/OtherNavbar';
 import  Footer  from './components/footer';
-import '../css/app.css';
-import "../css/navbar.css";
-import "../css/footer.css"; 
 import  HelpPage from './screens/helpPage';
 import Test from './screens/Test';
 import { json } from 'stream/consumers';
 import useBasket from './hooks/useBasket';
+import '../css/app.css';
+import "../css/navbar.css";
+import "../css/footer.css"; 
+import AuthenticationModal from './components/auth';
 
 function App() {
 const location = useLocation();
 const { cartItems, onAdd , onRemove,onDelate, onDeleteAll} = useBasket();
+        
+const [signupOpen , setSignupOpen] = useState<boolean>(false);
+const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+/** HANDLARS **/
+
+const handlerSigupClose= () => setSignupOpen(false);
+const handlerLoginClose= () => setLoginOpen(false);
+
   return (  
      <>
         {location.pathname === "/" ? (
@@ -53,6 +63,12 @@ const { cartItems, onAdd , onRemove,onDelate, onDeleteAll} = useBasket();
           </Route>
         </Switch>
         <Footer/>
+        <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handlerLoginClose}
+        handleSignupClose={handlerSigupClose}
+        />
       </>
 
   );
